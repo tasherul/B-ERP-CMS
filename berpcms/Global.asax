062d90b12e18f10455c460b604ff4d75460f7951
Tasherul Islam <47455913+tasherul@users.noreply.github.com>
2020-04-25 23:48:30 +0600

    Add project files.

---
+++ berpcms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="berpcms.MvcApplication" Language="C#" %>
---
+++ berpcms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(berpcms.Startup))]
+namespace berpcms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
